e967e48090cd504cbd22b942b93335f93669de9a
Heronakemito <akemitodido@gmail.com>
2022-10-18 08:22:14 +0200

    site des devs

---
+++ main.js
@@ -0,0 +1,30 @@
+//nav bar scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY >75)
+})
+
+
+//  NAV BAR 
+
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display ="none";
+})
+
+// CLOSE
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display ="inline-block";
+
+}
+
+closeBtn.addEventListener('click', closeNav)
